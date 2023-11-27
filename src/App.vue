87205a9c5a00c90da7d879ecf91eee2724c6dcb0
MarcoGranata9 <granatam9900@gmail.com>
2023-11-27 15:55:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios"
+import {store} from "./store.js"
+ 
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    created() {
+        axios.get(this.store.apiUrl).then((resp)=> {
+            this.store.cards = resp.data.results;
+        })
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
